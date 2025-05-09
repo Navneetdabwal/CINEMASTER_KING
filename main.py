a68b25d3b52f09220599fe6172c794ace07c22d7
Navneetdabwal <dabwalnavneet@gmail.com>
2025-05-09 13:56:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -43,3 +43,6 @@ app = ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, movie_handler))
 app.run_polling()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
