e78fbbceafc5b3e1c5ab84975e32d9f41f569826
Navneetdabwal <dabwalnavneet@gmail.com>
2025-05-10 13:14:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import os
 import asyncio
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
-from scraper import search_movie_links
+from scraper import search_google_link
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        "🎬 Welcome to Internet Movie Bot!\n\n"
-        "Send any movie name and I’ll search the internet for a working streaming link.\n"
+        "🎬 Welcome to CINEMASTRO Bot!\n\n"
+        "Send any movie name and I’ll search Google for a working streamable link.\n"
         "____________________________________\n"
         "👨‍💻 *Bot Developer:* `『𝙉𝘼𝙑𝙉𝙀𝙀𝙏 𝘿𝘼𝘽𝙒𝘼𝙇』`\n"
         "____________________________________",
@@ -18,16 +18,16 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def movie_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     movie_name = update.message.text
-    await update.message.reply_text("🔍 Searching internet for: " + movie_name)
+    await update.message.reply_text("🔍 Searching Google for: " + movie_name)
 
-    link = search_movie_links(movie_name)
+    link = search_google_link(movie_name)
 
     if not link:
-        await update.message.reply_text("❌ No streamable link found on the internet.")
+        await update.message.reply_text("❌ No streamable link found on Google.")
         return
 
     msg = await update.message.reply_text(
-        f"🎬 Found Link:\n{link}\n\n⚠️ Auto-fetched from public internet.\n⏱️ This message will delete in 1 minute."
+        f"🎬 Found Link:\n{link}\n\n⚠️ Auto-fetched from Google.\n⏱️ This message will delete in 1 minute."
     )
     await asyncio.sleep(60)
     try:
